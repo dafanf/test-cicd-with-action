e273d010410fe8ade645f756cb94d305b6c1c477
Dafa Nurul Fauziansyah <61896490+dafanf@users.noreply.github.com>
2024-01-25 21:12:24 +0700

    Add files via upload

---
+++ backend/util/jwt.js
@@ -0,0 +1,15 @@
+require('dotenv').config();
+const jwt = require("jsonwebtoken");
+
+module.exports.sign = async (user) => {
+  const token = await jwt.sign(
+    { id: user.id, username: user.username, email: user.email },
+    process.env.JWT_SECRET
+  );
+  return token;
+};
+
+module.exports.verify = async (token) => {
+  const decoded = await jwt.verify(token, process.env.JWT_SECRET);
+  return decoded;
+};
